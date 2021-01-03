73e12399f46a857f938a69ff7fabe977a2478646
Antti Pham <antti.pham@gmail.com>
2021-01-03 16:24:34 +0200

    testaamisen vuoksi default route

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ app.use(cors())
 // Kaikki staattiset tiedostot
 app.use('/cv', express.static('build/cv'))
 
+// Default route
+app.get('/', (req, res) => {
+  res.send('Hello World!').status(200)
+})
+
 module.exports = app
